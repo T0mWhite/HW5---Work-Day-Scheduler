1c03560f8d605f94e74c1776023def473d29231a
T0mWhite <tbw2.mail@gmail.com>
2021-11-17 21:34:08 -0600

    added event submit to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 setInterval(function() {
     let today = moment();
     $("#live-date").text(today.format("dddd, MMMM Do"));
@@ -5,30 +6,32 @@ setInterval(function() {
 
 // setInterval(deezNuts, 1000);
 // function deezNuts() {
-//     let today = moment();
-//     $("#live-date").text(today.format("dddd, MMMM Do"));
-// }
-
-// let currentTime = moment().format();
-
-let nine = document.querySelector("#nine");
-nine.time = 9;
-let ten = document.querySelector("#ten");
-ten.time = 10;
-let eleven = document.querySelector("#eleven");
-eleven.time = 11;
-let twelve = document.querySelector("#twelve");
-twelve.time = 12;
-let thirteen = document.querySelector("#thirteen");
-thirteen.time = 13;
-let fourteen = document.querySelector("#fourteen");
-fourteen.time = 14;
-let fifteen = document.querySelector("#fifteen");
-fifteen.time = 15;
-let sixteen = document.querySelector("#sixteen");
-sixteen.time = 16;
+    //     let today = moment();
+    //     $("#live-date").text(today.format("dddd, MMMM Do"));
+    // }
+    
+    // let currentTime = moment().format();
+    
+    let nine = document.querySelector("#nine");
+    nine.time = 9;
+    let ten = document.querySelector("#ten");
+    ten.time = 10;
+    let eleven = document.querySelector("#eleven");
+    eleven.time = 11;
+    let twelve = document.querySelector("#twelve");
+    twelve.time = 12;
+    let thirteen = document.querySelector("#thirteen");
+    thirteen.time = 13;
+    let fourteen = document.querySelector("#fourteen");
+    fourteen.time = 14;
+    let fifteen = document.querySelector("#fifteen");
+    fifteen.time = 15;
+    let sixteen = document.querySelector("#sixteen");
+    sixteen.time = 16;
 let seventeen = document.querySelector("#seventeen");
 seventeen.time = 17;
+
+
 
 let timeArray = [nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen];
 
@@ -64,15 +67,32 @@ console.log(schedTime);
 console.log("Current time is " + moment().hours());
 
 // for (let i = 0; i < timeArray.length; i++) {
-//     // schedTime = timeArray[i];
+    //     // schedTime = timeArray[i];
 //     console.log("Time is " + timeArray[i].time);
 //     }
 
 
 // storage
 
+
 let eventStorage = [] || JSON.parse(window.localStorage.getItem("eventStorage"));
 
-let eventDescription = {
-    description: 
-}
+let nineFormSubmit = document.querySelector('#nine-form');
+
+
+nineFormSubmit.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let nineInput = document.querySelector('#nine-input');    
+    
+    let eventDescription = {
+        description: nineInput.value,
+        time: nine.time
+    }
+
+    localStorage.setItem('eventStorage', JSON.stringify(eventDescription));
+});
+
+// eventStorage.push(eventDescription);
+
+
+// let eventTime = document.querySelector('');
